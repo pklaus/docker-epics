a74befaa346bf8e7cfde0954eb214d133e9259ad
Philipp Klaus <klaus@physik.uni-frankfurt.de>
2018-01-25 14:52:08 +0100

    epics_synapps: 1st working version

---
+++ epics_synapps/debian-jessie/build_configuration.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+BUILDS = {
+    # tag name:                          template settings
+    'debian-jessie':                     {'base_img': 'pklaus/epics_base:debian-jessie', 'cross_build': False},
+    'resin-raspberry-pi-debian-jessie':  {'base_img': 'pklaus/epics_base:resin-raspberry-pi-debian-jessie', 'cross_build': True},
+    'resin-raspberrypi3-debian-jessie':  {'base_img': 'pklaus/epics_base:resin-raspberrypi3-debian-jessie', 'cross_build': True},
+}
