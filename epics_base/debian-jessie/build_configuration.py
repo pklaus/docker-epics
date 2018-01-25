35c2246fd17555b14e6ad30726e97de26eb7b3c6
Philipp Klaus <klaus@physik.uni-frankfurt.de>
2018-01-25 11:48:55 +0100

    debian-jessie build is without cross-compiler

--- epics_base/debian-jessie/build_configuration.py
+++ epics_base/debian-jessie/build_configuration.py
@@ -2,7 +2,7 @@
 
 BUILDS = {
     # tag name:                          template settings
-    'debian-jessie':                     {'base_img': 'debian:jessie', 'cross_build': True},
+    'debian-jessie':                     {'base_img': 'debian:jessie', 'cross_build': False},
     'resin-raspberry-pi-debian-jessie':  {'base_img': 'resin/raspberry-pi-debian:jessie', 'cross_build': True},
     'resin-raspberrypi3-debian-jessie':  {'base_img': 'resin/raspberrypi3-debian:jessie', 'cross_build': True},
 }
