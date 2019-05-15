7e84869745d07203171ce1292e2d4e5f1cfc468b
Philipp Klaus <philipp.l.klaus@web.de>
2019-05-15 16:26:00 +0200

    create versioned subfolders

--- epics_base/3.15.5_debian/build_configuration.py
+++ epics_base/3.15.5_debian/build_configuration.py
@@ -5,4 +5,7 @@ BUILDS = {
     'debian-jessie':                     {'base_img': 'debian:jessie', 'epics_host_arch': 'linux-x86_64', 'cross_build': False},
     'resin-raspberry-pi-debian-jessie':  {'base_img': 'resin/raspberry-pi-debian:jessie', 'epics_host_arch': 'linux-arm', 'cross_build': True},
     'resin-raspberrypi3-debian-jessie':  {'base_img': 'resin/raspberrypi3-debian:jessie', 'epics_host_arch': 'linux-arm', 'cross_build': True},
+    'debian-stretch':                    {'base_img': 'debian:stretch', 'epics_host_arch': 'linux-x86_64', 'cross_build': False},
+    'resin-raspberry-pi-debian-stretch': {'base_img': 'resin/raspberry-pi-debian:strech', 'epics_host_arch': 'linux-arm', 'cross_build': True},
+    'resin-raspberrypi3-debian-strech':  {'base_img': 'resin/raspberrypi3-debian:strech', 'epics_host_arch': 'linux-arm', 'cross_build': True},
 }
