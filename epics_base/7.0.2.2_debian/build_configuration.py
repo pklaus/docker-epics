0c1fbb27756e87b892bdb0ef5227971f6f16cfe3
Philipp Klaus <philipp.l.klaus@web.de>
2019-05-15 16:30:55 +0200

    EPICS base 7.0.2.2 added

---
+++ epics_base/7.0.2.2_debian/build_configuration.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+BUILDS = {
+    # tag name:            template settings
+    '7.0.2.2_debian':      {'base_img': 'debian:stretch', 'epics_host_arch': 'linux-x86_64', 'cross_build': False},
+    '7.0.2.2_rpi_debian':  {'base_img': 'balenalib/raspberry-pi-debian:stretch', 'epics_host_arch': 'linux-arm', 'cross_build': True},
+    '7.0.2.2_rpi3_debian': {'base_img': 'balenalib/raspberrypi3-debian:stretch', 'epics_host_arch': 'linux-arm', 'cross_build': True},
+}
